b492bbf5019df162c208857af8ec087c63370510
Zhonghao Shi <zhonghas@usc.edu>
2018-09-26 14:28:03 -0700

    new posts

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import $ from 'jquery';
 
 export default Route.extend({
-  model(params) {
+  model() {
    let url = `https://www.reddit.com/r/javascript.json`;
    return $.getJSON(url);
  }
